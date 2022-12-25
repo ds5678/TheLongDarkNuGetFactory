20169ec905ab65498fb33a4bc4fbb1831f43494b
Jeremy Pritts <jrpritts7@gmail.com>
2022-12-25 13:44:14 -0500

    Initial package release

--- TheLongDarkNuGetFactory/NuSpec.cs
+++ TheLongDarkNuGetFactory/NuSpec.cs
@@ -21,8 +21,13 @@ internal static class NuSpec
 		    <repository type="git" url="{PackageInformation.RepositoryLink}" />
 		    <dependencies>
 		      <group targetFramework="net6.0">
-		        <dependency id="AsmResolver.PE" version="5.0.0" exclude="Build,Analyzers" />
-		        <dependency id="System.Text.Json" version="6.0.7" exclude="Build,Analyzers" />
+		        <dependency id="AsmResolver.DotNet" version="5.0.0" exclude="Build,Analyzers" />
+		        <dependency id="AssetRipper.VersionUtilities" version="1.2.1" exclude="Build,Analyzers" />
+		        <dependency id="AssetsTools.NET" version="3.0.0-preview1" exclude="Build,Analyzers" />
+		        <dependency id="HarmonyX" version="2.10.1" exclude="Build,Analyzers" />
+		        <dependency id="Il2CppInterop.Common" version="1.4.3" exclude="Build,Analyzers" />
+		        <dependency id="Il2CppInterop.Runtime" version="1.4.3" exclude="Build,Analyzers" />
+		        <dependency id="LavaGang.MelonLoader" version="0.6.0" exclude="Build,Analyzers" />
 		      </group>
 		    </dependencies>
 		  </metadata>
--- TheLongDarkNuGetFactory/PackageInformation.cs
+++ TheLongDarkNuGetFactory/PackageInformation.cs
@@ -5,10 +5,10 @@ internal static class PackageInformation
 	public const string Authors = "ds5678";
 	public const string Name = "ds5678.Modding.TLD.Il2CppAssemblies.Windows";
 	/// <summary>
-	/// 2.04, nuget release 0
+	/// 2.06, reserved number, nuget release 0
 	/// </summary>
-	public const string Version = "2.4.0";
+	public const string Version = "2.6.0.0";
 	public const string Description = "Il2Cpp assemblies for modding The Long Dark by Hinterland Games.";
-	public const string Tags = "modding mod unity tld";
+	public const string Tags = "modding mod unity tld melonloader";
 	public const string RepositoryLink = @"https://github.com/ds5678/TheLongDarkNuGetFactory";
 }
